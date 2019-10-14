7cc6838bc0c024e68709ac659bb3b103f49cc780
Clayton Hart <claytonhart123@yahoo.com>
2019-10-14 14:09:49 -0700

    Updates

---
+++ precache-manifest.ef86860d0289cfd68be4cc0687de76c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ced6a66425056b8b0d497e417033e37",
+    "url": "/music-playlist-converter/index.html"
+  },
+  {
+    "revision": "c6de11ac4242a5998a41",
+    "url": "/music-playlist-converter/static/css/main.897cab2d.chunk.css"
+  },
+  {
+    "revision": "bc4f2034555f265cc129",
+    "url": "/music-playlist-converter/static/js/2.75c54901.chunk.js"
+  },
+  {
+    "revision": "c6de11ac4242a5998a41",
+    "url": "/music-playlist-converter/static/js/main.102093f2.chunk.js"
+  },
+  {
+    "revision": "cfce8d38f4f32f972cb1",
+    "url": "/music-playlist-converter/static/js/runtime-main.f2b9b08e.js"
+  },
+  {
+    "revision": "0b1251b6ba181d08a599ca2a06a24cc5",
+    "url": "/music-playlist-converter/static/media/mariah-ashby-4p0C_OiXNiM-unsplash.0b1251b6.jpg"
+  }
+]);
